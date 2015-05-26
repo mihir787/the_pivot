4c3c2e576c5b0acee99b0190e78641db0f968550
Tess Griffin <tessjgriffin@gmail.com>
2015-05-25 21:15:34 -0600

    Add store edit functionality for store admin users
    Store admin users need to edit their stores due to the pivot spec
    Links in the nav bar for All Photos and Create photos were broken due to not passing in the store slug
    We should have had tests for the nav bar links
    
    The reason why we changed the route to be :id instead of :store was because this is editing a store specifically,
    and the store id is the same regardless of the slug for :store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,4 +2,29 @@ class StoresController < ApplicationController
   def index
     @stores = Store.all
   end
+
+  def edit
+    @store = store
+  end
+
+  def update
+    @store = store
+    if @store.update(store_params)
+      flash[:notice] = "#{@store.name} Updated"
+      redirect_to store_admin_dashboard_path(store, current_user)
+    else
+      flash[:error] = @store.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :description, :image)
+  end
+
+  def store
+    @store ||= Store.find_by!(slug: params[:id])
+  end
 end
